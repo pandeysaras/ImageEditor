8983b242cd5638546897e6f353dffc621aafccb3
Saraswati Pandey <sarasmahi@outlook.com>
2023-05-21 13:57:33 +0530

    Image editor completed

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:image_editor/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Image Editor',
+      theme: ThemeData.light(),
+      home: const HomeScreen(),
+    );
+  }
+}
+
